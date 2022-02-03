370f7dadb647b4d8a773d9fa134acc6ef462b4d7
TsalachSnow <prosper.amalaha@decagon.dev>
2022-02-03 18:36:48 +0100

    LinkedList Practice

--- src/main/java/com/example/linkedlist/linkedList1.java
+++ src/main/java/com/example/linkedlist/linkedList1.java
@@ -1,6 +1,5 @@
 package com.example.linkedlist;
 
-import java.util.LinkedList;
 import lombok.Data;
 
 @Data
@@ -25,11 +24,63 @@ class LinkedList1 {
         return node;
     }
 
+    public Node addAtIndex(int index, int data){
+        Node temp = head;
+        Node node1 = new Node(data);
+        if(index > size){
+          throw new IndexOutOfBoundsException("out of bound at " +index);
+        }else{
+
+            for(int i = 0; i < size; i++){
+                if (i == index-1){
+                  node1.setNextNode(temp.getNextNode());
+                  temp.nextNode = node1;
+                }
+                    temp = temp.getNextNode();
+            }
+        }
+        return node1;
+    }
+     public Node addNextAtHead(int data){
+        Node newNode = new Node(data);
+        if(head == null){
+            head = newNode;
+            tail = newNode;
+            size++;
+        }else{
+            newNode.setNextNode(head);
+            head = newNode;
+            size++;
+        }
+        return newNode;
+     }
+
+
+
+    @Override
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+        sb.append("[ ");
+        Node temp = head;
+        while (temp != null) {
+            sb.append(temp.data);
+            if (temp.getNextNode() != null) {
+                sb.append(", ");
+            }
+            temp = temp.getNextNode();
+        }
+        sb.append(" ]");
+        sb.append("size: " +size +" Node");
+        return sb.toString();
+    }
+
     public static void main(String[] args) {
         LinkedList1 link = new LinkedList1();
         link.add(40);
         link.add(20);
         link.add(30);
+        link.addNextAtHead(2);
+        link.addAtIndex(2,203);
         System.out.println(link.getHead().getData());
         System.out.println(link);
     }
