9584fe2b5d915ae2def6c2461654efe2a2731fb8
TsalachSnow <prosper.amalaha@decagon.dev>
2022-02-05 14:11:26 +0100

    insertion of node in a circularSinglyLinkedList and transversing through a circularSinglyLinkedList

--- src/main/java/com/example/linkedlist/CircularSingularLinkedList/CircularSinglyLinkedList.java
+++ src/main/java/com/example/linkedlist/CircularSingularLinkedList/CircularSinglyLinkedList.java
@@ -15,4 +15,46 @@ public class CircularSinglyLinkedList {
         size = 1;
         return head;
     }
+    // insertion in a CircularSinglyLinkedList any the beginning, at any poin and at the last position
+    public void insertCSLL(int nodeValue, int location){
+        Node node = new Node();
+        node.value = nodeValue;
+        if(head == null){
+            createCSLL(nodeValue);
+            return;
+        }else if(location == 0){ //insertion at the beginning
+            node.next = head;
+            head = node;
+            tail.next = head;
+        }else if(location >= size){ //isertion at the end
+            tail.next = node;
+            tail = node;
+            tail.next = head;
+        }else{ //insertion at any point
+           Node tempNode = head;
+           int index = 0;
+           while(index < location - 1){
+               tempNode = tempNode.next;
+               index++;
+           }
+           node.next = tempNode.next;
+           tempNode.next = node;
+        }
+        size++;
+    }
+    //transversal method
+    public void transverseCSLL(){
+        if(head != null){
+            Node tempNode = head;
+            for(int i = 0; i < size; i++){
+                System.out.println(tempNode.value);
+                if ((i != size - 1)) {
+                    System.out.println("->");
+                }
+                tempNode = tempNode.next;
+                }
+            }else{
+            System.out.println("\n CSLL does not exist");
+        }
+    }
 }
--- src/main/java/com/example/linkedlist/CircularSingularLinkedList/Main.java
+++ src/main/java/com/example/linkedlist/CircularSingularLinkedList/Main.java
@@ -1,13 +1,32 @@
 package com.example.linkedlist.CircularSingularLinkedList;
 
-import com.example.linkedlist.SinglyLinkedList.SinglyLinkedList;
 
 public class Main {
     public static void main(String[] args) {
         //creation of the first node for a circularSinglyLinkedList
-        CircularSinglyLinkedList csll = new CircularSinglyLinkedList();
-        csll.createCSLL(5);
-        System.out.println(csll.head.value);
-        System.out.println(csll.head.next.value);
+//        CircularSinglyLinkedList csll = new CircularSinglyLinkedList();
+//        csll.createCSLL(5);
+//        System.out.println(csll.head.value);
+
+
+        //insertion of node in a circularSinglyLinkedList
+//        CircularSinglyLinkedList csll1 = new CircularSinglyLinkedList();
+//        csll1.createCSLL(2);
+//        csll1.insertCSLL(4,1);
+//        csll1.insertCSLL(6,2);
+//        csll1.insertCSLL(7,8);
+//        System.out.println(csll1.head.value);
+//        System.out.println(csll1.head.next.value);
+//        System.out.println(csll1.head.next.next.value);
+//        System.out.println(csll1.tail.value);
+
+
+        //transverse through a circularSinglyLinkedList
+        CircularSinglyLinkedList csll2 = new CircularSinglyLinkedList();
+        csll2.createCSLL(5);
+        csll2.insertCSLL(4,0);
+        csll2.insertCSLL(6,1);
+        csll2.insertCSLL(7,8);
+        csll2.transverseCSLL();
 }
 }
